271406739181c1165150b9c6ddcc01199f664159
zhaoyu <94742242@qq.com>
2025-02-27 16:31:41 +0800

    第14天作业 使用 Viem 编写 ts 脚本查询Ethereum链上最近100个区块链内的 USDC Transfer记录 监听USDC 的Transfer Pending 交易

--- src/day10_viem/client.ts
+++ src/day10_viem/client.ts
@@ -1,7 +1,13 @@
-import { createPublicClient, http } from 'viem'
+import {createPublicClient, http, webSocket} from 'viem'
 import { mainnet } from 'viem/chains'
+
 
 export const publicClient = createPublicClient({
     chain: mainnet,
     transport: http()
+})
+
+export const publicWsClient = createPublicClient({
+    chain: mainnet,
+    transport: webSocket('wss://ethereum-rpc.publicnode.com')
 })
